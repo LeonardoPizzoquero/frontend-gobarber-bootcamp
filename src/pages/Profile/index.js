bb2ad4abd3d0bfb63653b4a19de208c7cb3d2927
Leonardo Pizzoquero <leonardo.pizzoquero@outlook.com>
2020-01-13 01:44:35 -0300

    Finished Frontend ReactJS

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="text" name="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de Senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
 }
